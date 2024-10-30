bf16e528745cef244d561d3acf48d3064bb72f7c
sehdevkumar <kumarsehdev600@gmail.com>
2024-10-30 10:08:49 +0530

    angular material added with tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatSelectModule} from "@angular/material/select"
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSelectModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-18-plus-all-in-setup';
 }
+
+
+
