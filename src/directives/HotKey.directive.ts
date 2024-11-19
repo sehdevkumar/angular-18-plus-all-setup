200bf2049e3e03641e2378cef1cad3eab6c1f59d
sehdev <sehdev@atai.ai>
2024-11-19 15:30:16 +0530

    hotkey service

--- src/directives/HotKey.directive.ts
+++ src/directives/HotKey.directive.ts
@@ -1,33 +1,20 @@
-import { Directive, ElementRef, inject, Renderer2 } from "@angular/core";
+import {
+  Component,
+  Directive,
+  ElementRef,
+  inject,
+  Renderer2,
+} from '@angular/core';
 
-/**
- * An Angular directive that listens for keydown events on an element and logs the pressed key and a predefined hotkey type to the console.
- *
- * @selector [appHotKey]
- * @standalone true
- * @inputs ['hotKeyType']
- */
 @Directive({
   selector: '[appHotKey]',
   standalone: true,
   inputs: ['hotKeyType'],
 })
 export class HotKeyDirective {
-  /**
-   * The Renderer2 service used to listen for keydown events.
-   */
   rr = inject(Renderer2);
-
-  /**
-   * The predefined hotkey type.
-   */
   hotKeyType: string = '';
 
-  /**
-   * Initializes the directive by injecting the Renderer2 and ElementRef services, and sets up a keydown event listener on the element.
-   *
-   * @param {ElementRef} element - The element to listen for keydown events on.
-   */
   constructor(private element: ElementRef) {
     this.rr.listen(
       this.element.nativeElement,
@@ -36,12 +23,24 @@ export class HotKeyDirective {
     );
   }
 
-  /**
-   * Logs the pressed key and the predefined hotkey type to the console when a keydown event occurs.
-   *
-   * @param {KeyboardEvent} event - The keydown event.
-   */
   private handleKeyDown(event: KeyboardEvent): void {
-    console.log(`Key pressed: ${event.key}`, this.hotKeyType);
+    console.log(`Key pressed: ${event.key}`,this.hotKeyType);
   }
 }
+
+@Component({
+  selector: 'app-hot-key',
+  standalone: true,
+  template: `
+    <div
+      class="w-full h-full grid justify-center items-center bg-slate-600"
+      appHotKey
+      hotKeyType="Enter"
+      tabindex="0"
+    >
+      Hotkey works!
+    </div>
+  `,
+  imports: [HotKeyDirective],
+})
+export class HotKeyComponent {}
