4b2461b22560709145c07ea0b8ecc2d20fc7ceb1
sehdev <sehdev@atai.ai>
2024-11-20 11:27:06 +0530

    hii

--- src/pipes/transformString.pipe.ts
+++ src/pipes/transformString.pipe.ts
@@ -7,7 +7,7 @@ import { Component, Pipe } from "@angular/core";
 
 export class TransformStringPipe{ 
     transform(value:string){
-        return value.toUpperCase();
+        return value.toUpperCase().padEnd(10,".");
     }
 }
 
@@ -15,9 +15,13 @@ export class TransformStringPipe{
 
 
 @Component({
-    standalone:true,
-    template:`
-    <h1>{{'hello' | transformString}}</h1>
+    selector: 'app-transform-string',
+    standalone: true,
+    template: `
+    <div class="bg-white rounded-lg shadow-lg p-4 flex-col  flex w-full h-full items-center justify-center">
+        <p class="text-3xl font-bold animate-bounce">Pipe works!</p>
+        <h1 class="text-4xl font-bold animate-pulse text-green-600 text-clip clear-right">{{ 'hello' | transformString }}</h1>
+    </div>
     `,
     imports: [TransformStringPipe]
 })
